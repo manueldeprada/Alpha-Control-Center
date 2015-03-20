600cbfa4c3d440a5861c75666bb92c0f27754b95
prada <prada@prada-pc>
2015-03-20 17:13:51 +0100

    initial commit 2

---
+++ src/arduino/control/center/main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arduino.control.center;
+
+/**
+ *
+ * @author prada
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      mainGUI main = new mainGUI();
+      main.setVisible(true);// TODO code application logic here
+    }
+    
+}
