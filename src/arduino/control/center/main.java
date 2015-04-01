58401ad0c8da39875bc4e56375cbb1ffe5d18894
prada <prada@prada-pc>
2015-04-01 12:51:28 +0200

    -automatically get prefered laf -fixed saved colour

--- src/arduino/control/center/main.java
+++ src/arduino/control/center/main.java
@@ -22,7 +22,7 @@ public class main {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Windows".equals(info.getName())) {
+                if (getLaF().equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
@@ -45,6 +45,45 @@ public class main {
             }
         });
         
+        
+        
     }
-    
+    public static String getLaF(){
+            if (isWindows()) {
+			return "Windows";
+		} else if (isMac()) {
+			return "Win";
+		} else if (isUnix()) {
+			return "GTK+";
+		} else if (isSolaris()) {
+			return "Win";
+		} else {
+                         return "Win";
+		}
+       
+        }
+    private static String OS = System.getProperty("os.name").toLowerCase();
+    public static boolean isWindows() {
+ 
+		return (OS.indexOf("win") >= 0);
+ 
+	}
+ 
+	public static boolean isMac() {
+ 
+		return (OS.indexOf("mac") >= 0);
+ 
+	}
+ 
+	public static boolean isUnix() {
+ 
+		return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
+ 
+	}
+ 
+	public static boolean isSolaris() {
+ 
+		return (OS.indexOf("sunos") >= 0);
+ 
+	}
 }
